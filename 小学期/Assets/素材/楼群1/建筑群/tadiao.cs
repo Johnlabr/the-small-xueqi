9f7036987d93a912ded3400a9686ee0129a527d0
Johnlabr <58999454+Johnlabr@users.noreply.github.com>
2020-08-22 23:33:23 +0800

    项目导入
    把UNITY暂有的资源导入项目

---
+++ 小学期/Assets/素材/楼群1/建筑群/tadiao.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tadiao : MonoBehaviour
+{
+    
+    //public int count = 0;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up*0.1f, Space.World);
+        if (transform.rotation.y > 90)
+        {
+            transform.Rotate(0, 0, 0);
+        }
+        return;
+        
+
+    }
+}
